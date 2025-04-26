a20fd545f7780efd0049a430b1aa38807c185a4b
RespectedFlea <110356532+RespectedFlea@users.noreply.github.com>
2025-04-26 12:53:22 -0400

    Added Comments

--- Assets/_Project/Scripts/Ui/Room/EggButtonUI.cs
+++ Assets/_Project/Scripts/Ui/Room/EggButtonUI.cs
@@ -1,6 +1,10 @@
 using UnityEngine;
 using CritterPetz;
 
+/// <summary>
+/// Handles clicking an egg button in the RoomEggSelector UI.
+/// Sends the selected EggData back to RoomEggSelector.
+/// </summary>
 public class EggButtonUI : MonoBehaviour
 {
     private EggData eggData;
